61ba32506219a855760eb7a4f61a512d3394b061
Vijaya Malla <mallabiju@gmail.com>
2022-06-14 07:46:49 +0545

    Class and Object

---
+++ console_classnobj/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace console_classnobj
+{
+    class Student
+    {
+        string name; //data member
+        int roll; //data member
+
+        public void helloHi()
+        {
+            Console.WriteLine("Hello all how are you?");
+        }
+        static void Main(string[] args)
+        {
+            Student st1 = new Student();
+            st1.name = "Ram Thapa";
+            st1.roll = 6;
+            st1.helloHi();
+            Console.WriteLine("The name is " + st1.name + ".");
+            Console.WriteLine("The roll number is " + st1.roll + "."); 
+        }
+    }
+}
---
+++ console_classnobjtwo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace console_classnobjtwo
+{
+    public class Student{
+        public string name;
+        public int roll;
+
+        public void insert(string n, int r){
+            name = n;
+            roll = r;
+        }
+        public void display(){
+            Console.WriteLine(roll+ " " + name);
+        }
+    }
+    public class TestStudent
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student();
+            Student st2 = new Student();
+            st1.insert("Ram",6);
+            st1.display();
+            st2.insert("Sita",7);
+            st2.display();
+        }
+    }
+}
