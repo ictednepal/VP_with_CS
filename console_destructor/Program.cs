3441350b201ab8f2697ff0cb1c51f824d513c79d
Vijaya Malla <mallabiju@gmail.com>
2022-06-20 07:12:12 +0545

    Destructor

---
+++ console_destructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace console_destructor
+{
+    class Student{
+        public Student(){
+            Console.WriteLine("Constructor created");
+        }
+        ~Student(){
+            Console.WriteLine("Destructor calling");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+        }
+    }
+}
