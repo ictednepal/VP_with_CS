b3657ed9b3923a58f3f37e6a8b0fb61ae4bf6e62
Vijaya Malla <mallabiju@gmail.com>
2022-06-21 07:43:23 +0545

    Static Class, Properties and Methods

---
+++ console_static/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace console_static
+{
+    public static class MyMath{
+        public static float pi = 3.14f;
+        public static int cube(int a){
+            return (a*a*a);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The value of pi is ="+MyMath.pi);
+            Console.WriteLine("The cube is = "+MyMath.cube(5));
+        }
+    }
+}
