35c796771f7a536b12b7c02bb73eae066a551032
Vijaya Malla <mallabiju@gmail.com>
2022-07-14 07:32:35 +0545

    C# String

---
+++ console_stringbuilder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace console_stringbuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder s = new StringBuilder("Sanothimi",25); 
+            Console.WriteLine(s);
+        }
+    }
+}
